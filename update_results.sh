cd5465f7bff9af62ac64b6c2b64da2f60a6171cc
Marc Harper <marc.harper@gmail.com>
2015-10-12 22:22:02 -0700

    Update assets

--- update_results.sh
+++ update_results.sh
@@ -2,15 +2,17 @@
 # Update the noisy tournaments
 echo "-- Regenerating noisy tournament figures"
 # Run the noisy tournament
-./run_axelrod --noise 0.05 --rc -p 0 -o assets
+./run_axelrod --noise 0.05 --rc -p 0 -o assets -r 100 --xc
 # Copy these figures over, renaming as appropriate
 mv assets/strategies_payoff.svg assets/strategies_payoff_noise_5.svg
 mv assets/strategies_boxplot.svg assets/strategies_boxplot_noise_5.svg
 mv assets/strategies_winplot.svg assets/strategies_winplot_noise_5.svg
+mv assets/strategies_sdvplot.svg assets/strategies_sdvplot_noise_5.svg
+mv assets/strategies_pdplot.svg assets/strategies_pdplot_noise_5.svg
 
 # Running noiseless tournament
 echo "-- Running Axelrod, regenerating cache, results, and figures"
 # Also Copy the results and figures over to assets
-./run_axelrod --rc -p 0 -o assets
+./run_axelrod --rc -p 0 -o assets -r 100 --xc
 
 echo "Be sure to git commit changes to update the docs!"
