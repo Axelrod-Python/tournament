47cc0431f40f7f330fae59918a911a7aa15afef4
Vince Knight <vincent.knight@gmail.com>
2016-04-02 15:28:13 +0100

    Adding prob end to the update script.

--- update_results.sh
+++ update_results.sh
@@ -6,7 +6,7 @@ repetitions="$2"
 # Update the noisy tournaments
 echo "-- Regenerating noisy tournament figures"
 # Run the noisy tournament
-python3 run_axelrod --noise 0.05 --rc -p $processes -o assets -r $repetitions --xc
+python run_axelrod --noise 0.05 --rc -p $processes -o assets -r $repetitions --xc
 # Copy these figures over, renaming as appropriate
 mv assets/strategies_payoff.svg assets/strategies_payoff_noise_5.svg
 mv assets/strategies_boxplot.svg assets/strategies_boxplot_noise_5.svg
@@ -17,6 +17,11 @@ mv assets/strategies_pdplot.svg assets/strategies_pdplot_noise_5.svg
 # Running noiseless tournament
 echo "-- Running Axelrod, regenerating cache, results, and figures"
 # Also Copy the results and figures over to assets
-./run_axelrod --rc -p $processes -o assets -r $repetitions --xc
+python run_axelrod --rc -p $processes -o assets -r $repetitions --xc
+
+# Running prob end tournament with p_end=0.1
+echo "-- Running Prob End Axelrod, regenerating cache, results, and figures"
+# Also Copy the results and figures over to assets
+python run_axelrod -pe 0.1 -p $processes -o assets -r $repetitions --xc
 
 echo "Be sure to git commit changes to update the docs!"
