98ba8e7b97764f51e6f73f1092ec2334fed7626b
Vince Knight <vincent.knight@gmail.com>
2016-04-02 22:41:50 +0100

    Results script.

--- update_results.sh
+++ update_results.sh
@@ -6,7 +6,8 @@ repetitions="$2"
 # Update the noisy tournaments
 echo "-- Regenerating noisy tournament figures"
 # Run the noisy tournament
-python run_axelrod --noise 0.05 --rc -p $processes -o assets -r $repetitions --xc
+#python3.5 run_axelrod --noise 0.05 -p $processes -o assets -r $repetitions --xc
+python3.5 run_axelrod --noise 0.05 -o assets -r $repetitions -t 20
 # Copy these figures over, renaming as appropriate
 mv assets/strategies_payoff.svg assets/strategies_payoff_noise_5.svg
 mv assets/strategies_boxplot.svg assets/strategies_boxplot_noise_5.svg
@@ -17,11 +18,13 @@ mv assets/strategies_pdplot.svg assets/strategies_pdplot_noise_5.svg
 # Running noiseless tournament
 echo "-- Running Axelrod, regenerating cache, results, and figures"
 # Also Copy the results and figures over to assets
-python run_axelrod --rc -p $processes -o assets -r $repetitions --xc
+#python3.5 run_axelrod -p $processes -o assets -r $repetitions --xc
+python3.5 run_axelrod -o assets -r $repetitions -t 20
 
 # Running prob end tournament with p_end=0.1
 echo "-- Running Prob End Axelrod, regenerating cache, results, and figures"
 # Also Copy the results and figures over to assets
-python run_axelrod -pe 0.1 -p $processes -o assets -r $repetitions --xc
+#python3.5 run_axelrod -pe 0.1 -p $processes -o assets -r $repetitions --xc
+python3.5 run_axelrod -pe 0.1 -o assets -r $repetitions
 
 echo "Be sure to git commit changes to update the docs!"
