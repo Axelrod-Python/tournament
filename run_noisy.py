2522388fcbbbf168a0c248196e415bf3621715bb
Vince Knight <vincent.knight@gmail.com>
2016-05-17 16:32:32 +0100

    Changing arguments for tournament scripts.

--- run_noisy.py
+++ run_noisy.py
@@ -11,7 +11,7 @@ filename = "data/strategies_noisy_interactions.csv"
 
 if __name__ == '__main__':
     players = [s() for s in axl.ordinary_strategies]
-    tournament = axl.Tournament(players, turns=turns, repetitions=repetitions, noise=noise, processes=processes)
+    tournament = axl.Tournament(players, turns=turns, repetitions=repetitions, noise=noise)
 
     random.seed(seed)  # Setting a seed
-    tournament.play(filename)
+    tournament.play(filename=filename, processes=processes)
--- run_probend.py
+++ run_probend.py
@@ -9,7 +9,7 @@ filename = "data/strategies_probend_interactions.csv"
 
 if __name__ == "__main__":
     players = [s() for s in axl.ordinary_strategies]
-    tournament = axl.ProbEndTournament(players, prob_end=prob_end, repetitions=repetitions, processes=processes)
+    tournament = axl.ProbEndTournament(players, prob_end=prob_end, repetitions=repetitions)
 
     random.seed(seed)  # Setting a seed
-    tournament.play(filename)
+    tournament.play(filename=filename, processes=processes)
