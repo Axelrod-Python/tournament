6d798a2bd586800fcb82a960e7144586c4ddabad
Vince Knight <vincent.knight@gmail.com>
2016-06-09 08:16:30 +0100

    Deleting the data file.

--- run_probend.py
+++ run_probend.py
@@ -1,5 +1,6 @@
 import axelrod as axl
 import random
+import os
 
 prob_end = .1
 repetitions = 100
@@ -8,8 +9,15 @@ seed = 1
 filename = "data/strategies_probend_interactions.csv"
 
 if __name__ == "__main__":
+    # Deleting the file if it exists
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
+
+    random.seed(seed)  # Setting a seed
+
     players = [s() for s in axl.ordinary_strategies]
     tournament = axl.ProbEndTournament(players, prob_end=prob_end, repetitions=repetitions)
 
-    random.seed(seed)  # Setting a seed
     tournament.play(filename=filename, processes=processes)
