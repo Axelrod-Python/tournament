d97daa4168f3191b42aba23b606709cb51611a87
Owen Campbell <owen.campbell@tanti.org.uk>
2017-03-22 12:49:29 +0000

    Add MetaWinner to parameterized players

--- players.py
+++ players.py
@@ -2,14 +2,20 @@
 import axelrod as axl
 
 # Include a list of parametrized strategies
-parametrized_players = [axl.Random(0.1),
-                        axl.Random(0.3),
-                        axl.Random(0.7),
-                        axl.Random(0.9),
-                        axl.GTFT(0.1),
-                        axl.GTFT(0.3),
-                        axl.GTFT(0.7),
-                        axl.GTFT(0.9)]
+parameterized_players = [
+	axl.Random(0.1),
+    axl.Random(0.3),
+    axl.Random(0.7),
+    axl.Random(0.9),
+    axl.GTFT(0.1),
+    axl.GTFT(0.3),
+    axl.GTFT(0.7),
+    axl.GTFT(0.9),
+    axl.MetaWinner(team=[
+        EvolvedHMM5, EvolvedLookerUp2_2_2, EvolvedFSM16, EvolvedANN5,
+        PSOGambler2_2_2, FoolMeOnce, DoubleCrosser, Gradual
+    ]),
+]
 
-players = [s() for s in axl.strategies] + parametrized_players
+players = [s() for s in axl.strategies] + parameterized_players
 players.sort(key=lambda p:p.__repr__())
