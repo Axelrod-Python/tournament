e4050b4883aabb0819da0296c05a66f0ac8da1c9
Vince Knight <vincent.knight@gmail.com>
2017-04-08 11:46:36 +0100

    Format players.

--- players.py
+++ players.py
@@ -4,14 +4,14 @@ import axelrod as axl
 # Include a list of parametrized strategies
 parameterized_players = [
 	axl.Random(0.1),
-    axl.Random(0.3),
-    axl.Random(0.7),
-    axl.Random(0.9),
-    axl.GTFT(0.1),
-    axl.GTFT(0.3),
-    axl.GTFT(0.7),
-    axl.GTFT(0.9),
-    axl.MetaWinner(team=[
+        axl.Random(0.3),
+        axl.Random(0.7),
+        axl.Random(0.9),
+        axl.GTFT(0.1),
+        axl.GTFT(0.3),
+        axl.GTFT(0.7),
+        axl.GTFT(0.9),
+        axl.MetaWinner(team=[
         axl.EvolvedHMM5, axl.EvolvedLookerUp2_2_2, axl.EvolvedFSM16,
         axl.EvolvedANN5, axl.PSOGambler2_2_2, axl.FoolMeOnce,
         axl.DoubleCrosser, axl.Gradual
